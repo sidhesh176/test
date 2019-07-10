967c15e0c6983440128ccd14a0fed6ffceed4924
yuvadany <toyuvaraj@gmail.com>
2019-07-10 22:31:07 +0530

    Lambda Expressions

---
+++ src/com/java/core/LambdaSam.java
@@ -0,0 +1,21 @@
+package com.java.core;
+
+public class LambdaSam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Why use Lambda Expression ========================= 
+		 * 1. To provide the implementation of Functional interface
+		 * 2. Less coding
+		 */
+		
+		// (argument-list) -> {body}
+		// 1) Argument-list: It can be empty or non-empty as well.
+		// 2) Arrow-token: It is used to link arguments-list and body of expression.
+		// 3) Body: It contains expressions and statements for lambda expression.
+		
+		System.out.println("Lambda Expressions Sample");
+	}
+
+}
