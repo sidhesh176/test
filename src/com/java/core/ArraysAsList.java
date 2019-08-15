61cb719032378137f65125bc8a7e0eb44b541363
yuvadany <toyuvaraj@gmail.com>
2019-08-15 12:09:13 +0530

    Arrays.asList(array)

--- src/com/java/core/ArraysAsList.java
+++ src/com/java/core/ArraysAsList.java
@@ -10,16 +10,23 @@ public class ArraysAsList {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String[] namesArray = new String[] { "Yuvi", "Kishore", "Raghav" };
 		List<Integer> values = Arrays.asList(1, 6, 8, 3, 6);
 		values.forEach(i -> System.out.println(i));
 		
-		List<String> names =  Arrays.asList("Yuvi","Kishore","Raghav");
+		System.out.println();
+		
+		List<String> names = Arrays.asList("Yuvi", "Kishore", "Raghav");
 		names.forEach(name -> System.out.println(name));
+		
+		System.out.println();
 		
 		Collections.sort(names);
 		names.forEach(name -> System.out.println(name));
-		
+
+		System.out.println();
+		List namesArrayList = Arrays.asList(namesArray);
+		namesArrayList.forEach(n -> System.out.println(n));
 	}
-	
 
 }
